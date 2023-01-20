b355dcde9a87bb17b88c165143b712980050285e
bluespada <pentingmain@gmail.com>
2023-01-20 16:51:05 +0800

    feat : add documentation

--- src/extension.rs
+++ src/extension.rs
@@ -1,9 +1,39 @@
 use serde::{Deserialize, Serialize};
 
+/// # Extension Metadata
+/// 
+/// Struct type to define extension as a openagro extension
+/// and specify extension metadata.
+/// 
+/// **Example:** 
+/// 
+/// 
+/// ```
+/// func metadata() -> ExtensionMetadata {
+///     ExtensionMetadata {
+///         extension_id: "openagro.your.extension.name".to_string(),
+///         name: "Your Extension Name".to_string(),
+///         description: Some("Your Extension Description".to_string()),
+///         category: Some("Your Extension Category"),
+///         author: "your name".to_string(),
+///         version: "your.app.version".to_string(),
+///         depends_on: vec![
+///             "other.extension.is.needed.to.run.this.extension".to_string(),    
+///         ],
+///         data: vec![
+///             include_str!("../path/to/yourfile.js").to_string(),
+///             include_str!("path/to/yourfile").to_string(),
+///         ],
+///         application: true,
+///     }
+/// }
+/// ```
+/// 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExtensionMetadata {
-    pub extension_id: String,
-    pub name: String,
+    pub extension_id: String, // extension id
+    pub name: String, // extension name
+    // extension description
     pub description: Option<String>,
     pub category: Option<String>,
     pub author: String,
